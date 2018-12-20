015632e7ef3322b6ccfc20d7d7a623e5ea56275f
Nikia Gubenkov <n.gubenkov@gmail.com>
2018-12-20 13:59:07 +0500

    return article changed

--- wikipedia.py
+++ wikipedia.py
@@ -2,29 +2,24 @@ import wikipediaapi
 import requests
 
 def return_article(name):
-        if(name == "Python programing" or name == "Python Language"):
-                return("is okay")
+        #if(name == "Python programing" or name == "Python Language"): return("is okay")
 
         wiki_wiki = wikipediaapi.Wikipedia('en')
         page_py = wiki_wiki.page(name)
         #print(page_py.sections[0])
         #print(page_py.text[:page_py.text.index("\n")] )
-        print_sections(page_py.sections)
-        return(page_py.text[:page_py.text.index("\n")])
+        sections = return_sections(page_py.sections)
+        return(page_py.text, sections)
        # return(page_py.sections)
 
-def print_sections(sections, level=0):
-
+def return_sections(sections, level=0, section = []):
+        section = []
         for s in sections:
+                section.append(s.title)
+                #print("%s " % ( s.title))
+                return_sections(s.sections, level + 1, section)
 
-                print("%s " % ( s.title))
-                print_sections(s.sections, level + 1)
-
-
-
-
-
-
+        return section
 
 def wiki_search(name):
         url = "https://en.wikipedia.org/w/api.php?action=query&origin=*&format=json&generator=search&gsrsearch='{}'".format(name)
@@ -35,4 +30,8 @@ def wiki_search(name):
 
 #result = requests.get('https://en.wikipedia.org/w/api.php', params=req).json()
 
-print(return_article("Python language") )
+articleText, sections = return_article("Russia")
+print(articleText[:articleText.index(sections[0])])
+#print(articleText)
+print(sections)
+
